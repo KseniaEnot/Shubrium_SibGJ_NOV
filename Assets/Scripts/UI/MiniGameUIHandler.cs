e1e9e3edc22c719cb9b98da3bc317b3d050e4dca
Megalolomania <136518339+Megalolomania@users.noreply.github.com>
2024-11-02 19:42:09 +0400

    Add MiniGame UI Handler

---
+++ Assets/Scripts/UI/MiniGameUIHandler.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class MiniGameUIHandler : MonoBehaviour
+{
+	[SerializeField] private string _requestedString = "Requested Coins: ";
+	[SerializeField] private string _lootedString = "Looted Coins: ";
+	[SerializeField] private TextMeshProUGUI _requestedCoinsText;
+	[SerializeField] private TextMeshProUGUI _lootedCoinsText;
+
+	private void OnEnable()
+	{
+		EventBus.OnRequiredCoinsUpdated += DrawRequestedCoins;
+		EventBus.OnLootedCoinsUpdated += DrawLootedCoins;
+	}
+
+	private void OnDisable()
+	{
+		EventBus.OnRequiredCoinsUpdated -= DrawRequestedCoins;
+		EventBus.OnLootedCoinsUpdated -= DrawLootedCoins;
+	}
+
+	private void DrawRequestedCoins(int value)
+	{
+		_requestedCoinsText.text = _requestedString + value.ToString();
+	}
+
+	private void DrawLootedCoins(int value)
+	{
+		_lootedCoinsText.text = _lootedString + value.ToString();
+	}
+}
