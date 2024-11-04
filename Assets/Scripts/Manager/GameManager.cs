95810a6315e82afc58e2bcb34432172f63aba990
Eugene <mr.difiko@yandex.ru>
2024-11-04 18:21:12 +0700

    sf

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 public class GameManager : Singleton<GameManager>
 {
     public DayManager Day;
+    public CharacterHolder Character;
     public CurrencyManager Currency;
     public InGameUIManager UI;
     public MiniGameManager MiniGame;
